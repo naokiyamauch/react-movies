dd9e4bb805d8cf81d93566064b4f8e4a5fe2a381
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-05-15 15:04:43 -0700

    feature: added debouncing the search for optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useDebounce } from 'react-use';
 import MovieCard from './components/MovieCard';
 import Search from './components/Search';
 import Spinner from './components/Spinner';
@@ -20,6 +21,11 @@ const App = () => {
 	const [errorMessage, setErrorMessage] = useState('');
 	const [movieList, setMovieList] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+	// Debounce the search term to prevent making too many API requests
+	// by waiting for the user to stop typing for 500ms
+	useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
 	const fetchMovies = async (query = '') => {
 		setIsLoading(true);
@@ -54,8 +60,8 @@ const App = () => {
 	};
 
 	useEffect(() => {
-		fetchMovies(searchTerm);
-	}, [searchTerm]);
+		fetchMovies(debouncedSearchTerm);
+	}, [debouncedSearchTerm]);
 
 	return (
 		<main>
